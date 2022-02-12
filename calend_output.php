052a6d64a76d193616f4fd9bca1558f691b6814e
ursa-majoris <96943472+ursa-majoris@users.noreply.github.com>
2022-02-12 22:28:56 +0600

    Update calend_output.php

--- calend_output.php
+++ calend_output.php
@@ -1,7 +1,10 @@
 <link rel='stylesheet' href='css/styles.css'>
 <p class="title"><b>Астрономический календарь г. Алматы</b></p>
+<!--<p class="title"><b>Астрономический календарь для дома пр. Ветеранов 25</b></p>-->
 <?php
 require "test.php";
+//include "sun.php";
+//require "sun.php";
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $year_id = (int)trim($_POST["year"]);
     if($year_id < 1800 or $year_id > 2200){
@@ -39,7 +42,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     $data_type = $_POST['type_data'];
 
-    if($year_id and $month_id and $data_type == 3){
+    if($year_id and $month_id and $data_type == 4){
         $g = array();
         $g = month_sid_time($year_id,$month_id);
         echo "<div align='center'>";
@@ -60,7 +63,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
         echo "</tbody></table>";
 
-        $sun = month_sun_time($year_id, $month_id);
+        $sun = month_sun_time_new($year_id, $month_id);
         $twi_astr = month_twi_time_astr($year_id, $month_id);
         $twi_nav = month_twi_time_nav($year_id, $month_id);
         $twi_civil = month_twi_time_civil($year_id, $month_id);
@@ -91,16 +94,40 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         echo "</tbody></table>";
         echo "</div>";
     }
+    elseif (($year_id and $month_id and $data_type == 3)){
+        $moon_rise_set = month_moon($year_id, $month_id);
+        $moon_phase = Moon_phase_month($year_id, $month_id);
+        $moon_type = moon_type($year_id, $month_id);
+        echo "<div align='center'>";
+        echo "Год: ".$year_id.", Месяц: ".$month_id;
+        echo "<p><b>Восход и заход Луны по местному времени (UTC +6 ч.), фазы</b></p>";
+
+        echo "<table align='center'>";
+        echo "<thead>
+                <tr>
+                <th align='center'> &nbsp Число &nbsp </th>
+                <th align='center'> &nbsp Восход Луны &nbsp </th>
+                <th align='center'> &nbsp Заход Луны &nbsp </th>
+                <th align='center'> &nbsp Фаза &nbsp </th>
+                <th align='center'> &nbsp Рост  &nbsp </th>
+                </tr>
+            </thead>
+            <tbody>";
+        for($i = 0; $i < count($moon_rise_set); $i++) {
+            echo "<tr><td align='center'>".($i + 1)."</td><td align='center'>".$moon_rise_set[$i][0]."</td><td align='center'>".$moon_rise_set[$i][1]."</td><td align='center'>".$moon_phase[$i]."</td><td align='center'>".$moon_type[$i]."</td></tr>";
+        }
+        echo "</tbody></table>";
+        echo "</div>";
+    }
     elseif (($year_id and $month_id and $data_type == 2)){
-        $sun = month_sun_time($year_id, $month_id);
+        $sun = month_sun_time_new($year_id, $month_id);
         $twi_astr = month_twi_time_astr($year_id, $month_id);
         $twi_nav = month_twi_time_nav($year_id, $month_id);
         $twi_civil = month_twi_time_civil($year_id, $month_id);
         echo "<div align='center'>";
         echo "Год: ".$year_id.", Месяц: ".$month_id;
-        echo "<p><b>&nbsp &nbsp</b></p>";
         echo "<p><b>Восход и заход Солнца, сумерки по местному времени (UTC +6 ч.)</b></p>";
-        echo "<p><b>&nbsp &nbsp</b></p>";
+
         echo "<table align='center'>";
         echo "<thead>
             <tr>
@@ -142,6 +169,32 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         for($i = 0; $i < count($g); $i++) {
             echo "<tr><td align='center'>".($i + 1)."</td><td align='center'>".$g[$i]."</td></tr>";
         }
+        echo "</tbody></table>";
+        echo "</div>";
+    }
+    elseif (($year_id and $data_type == 5)){
+        $sol_eq_jd = equinox_solstice_year_jd($year_id);
+        $sping = Date_time_from_jd($sol_eq_jd[0]);
+        $summer = Date_time_from_jd($sol_eq_jd[1]);
+        $fall = Date_time_from_jd($sol_eq_jd[2]);
+        $winter = Date_time_from_jd($sol_eq_jd[3]);
+        echo "<div align='center'>";
+        echo "Год: ".$year_id;
+        echo "<p>&nbsp &nbsp</p>";
+        echo "<table align='center'>";
+        echo "<thead>
+            <tr>
+            <th align='center'>  Тип  </th>
+            <th align='center'> &nbsp &nbsp Месяц &nbsp &nbsp </th>
+            <th align='center'> &nbsp &nbsp День &nbsp &nbsp </th>
+            <th align='center'> &nbsp &nbsp Время (UTC) &nbsp &nbsp </th>
+            </tr>
+            </thead>
+            <tbody>";
+        echo "<tr><td align='center'> Весеннее равноденствие: </td><td align='center'>".$sping[1]."</td><td align='center'>".$sping[2]."</td><td align='center'>".hours_to_sep($sping[6])."</td></tr>";
+        echo "<tr><td align='center'> Летнее солнцестояние: </td><td align='center'>".$summer[1]."</td><td align='center'>".$summer[2]."</td><td align='center'>".hours_to_sep($summer[6])."</td></tr>";
+        echo "<tr><td align='center'> Осеннее равноденствие: </td><td align='center'>".$fall[1]."</td><td align='center'>".$fall[2]."</td><td align='center'>".hours_to_sep($fall[6])."</td></tr>";
+        echo "<tr><td align='center'> Зимнее солнцестояние: </td><td align='center'>".$winter[1]."</td><td align='center'>".$winter[2]."</td><td align='center'>".hours_to_sep($winter[6])."</td></tr>";
         echo "</tbody></table>";
         echo "</div>";
     }
